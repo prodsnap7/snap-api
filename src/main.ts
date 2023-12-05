fecde1cac1ac056f4e063d4a68cbcf955f6c536f
Faadhil Hakim <faadhil@nexttrack.biz>
2023-12-04 21:50:06 -0600

    app listen

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,6 @@ async function bootstrap() {
   app.setGlobalPrefix('api/v1');
   app.enableCors();
   await app.register(multipart);
-  await app.listen(3000);
+  await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
