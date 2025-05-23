5dbcc6d4edb7a12402a06c56042e6e93a1cabf69
Faadhil Hakim <faadhil@nexttrack.biz>
2025-05-23 10:45:14 -0500

    logging setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,13 @@
 import { APP_GUARD } from '@nestjs/core';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { Module } from '@nestjs/common';
+import { Module, Logger } from '@nestjs/common';
 import { DesignsModule } from './designs/designs.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { BlocksModule } from './blocks/blocks.module';
 import { FontsModule } from './fonts/fonts.module';
 import { IconsModule } from './icons/icons.module';
 import { UploadsModule } from './uploads/uploads.module';
-import { BullModule } from '@nestjs/bull';
+import { BullModule, BullModuleOptions } from '@nestjs/bull';
 import { BlocksConsumer } from './blocks/blocks.consumer';
 import { PhotosModule } from './photos/photos.module';
 import { TemplatesModule } from './templates/templates.module';
@@ -34,13 +34,20 @@ import { UtilsModule } from './lib/utils/utils.module';
     BullModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => {
+        const logger = new Logger('BullModule');
         const redisUrl = configService.get<string>('REDIS_URL');
         if (!redisUrl) {
+          logger.error('REDIS_URL is not defined in environment variables');
           throw new Error('REDIS_URL is not defined in environment variables');
         }
-        return {
-          redis: redisUrl,
+
+        const bullOptions: BullModuleOptions = {
+          url: redisUrl,
         };
+
+        logger.log(`Attempting to configure Bull with Redis URL: ${redisUrl}`);
+
+        return bullOptions;
       },
       inject: [ConfigService],
     }),
